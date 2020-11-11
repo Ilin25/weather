ba64ddb918e0d141b6fedf71460fb3de7b4239e2
Rustam Ilin <rustamilin2507@gmail.com>
2020-11-11 18:08:50 +0300

    Создан каркас приложения

---
+++ src/main/java/ru/ilin/weather/WeatherApplication.java
@@ -0,0 +1,13 @@
+package ru.ilin.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ilin/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilin.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
