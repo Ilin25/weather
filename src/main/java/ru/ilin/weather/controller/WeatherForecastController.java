c50adfa9ce4b0869ff59f89e83453200e00e79c5
Rustam Ilin <rustamilin2507@gmail.com>
2020-12-06 15:28:44 +0300

    added a scheduler

--- src/main/java/ru/ilin/weather/controller/WeatherForecastController.java
+++ src/main/java/ru/ilin/weather/controller/WeatherForecastController.java
@@ -2,10 +2,12 @@ package ru.ilin.weather.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import ru.ilin.weather.entity.WeatherForecast;
 import ru.ilin.weather.service.WeatherForecastService;
-
+@EnableScheduling
 @RestController
 @RequestMapping("/")
 public class WeatherForecastController {
@@ -17,6 +19,8 @@ public class WeatherForecastController {
         this.weatherForecastService = weatherForecastService;
     }
 
+
+    @Scheduled(fixedDelay = 10000)
     @GetMapping("test")
     public WeatherForecast addWeatherForecast(){
         return weatherForecastService.addWeatherForecast();
